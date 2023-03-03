278cc78be6c1c0ca916633cee6a4aa65a6808fba
moeedrafique <alimoeed15@gmail.com>
2023-03-03 11:58:08 +0500

    V5

--- organizations/dash_apps/temp_vent_data.py
+++ organizations/dash_apps/temp_vent_data.py
@@ -61,74 +61,78 @@ today_end = now.replace(hour=23, minute=59, second=59, microsecond=999999) - tim
 occupant_records = mycol_sim.find({'ref_id': 'DMC02-CWS', 'timestamp': {'$gte': today_start, '$lte':today_end}}).sort('timestamp',-1)
 print(occupant_records)
 occu_dt = []
-for c in occupant_records:
-    occu_dt.append(c)
-# print(len(occu_dt))
-data = pd.DataFrame(occu_dt)
+if occu_dt:
+    for c in occupant_records:
+        occu_dt.append(c)
+    # print(len(occu_dt))
+    data = pd.DataFrame(occu_dt)
 
-main_data = data['data']
+    main_data = data['data']
 
-for i in main_data:
-    res = i["sf1_2"] + i["sf2_2"] + i["ahu_out"] + i['sg1_1'] + i['sg2_2'] + i['sg3_2'] + i['sg4_2'] + i['sg5_2'] + i['sg6_2']
-    mean_first_inlet = res / 9
-    yy.append(mean_first_inlet)
-# print(len(yy))
-# print(np.mean(yy))
+    for i in main_data:
+        res = i["sf1_2"] + i["sf2_2"] + i["ahu_out"] + i['sg1_1'] + i['sg2_2'] + i['sg3_2'] + i['sg4_2'] + i['sg5_2'] + i['sg6_2']
+        mean_first_inlet = res / 9
+        yy.append(mean_first_inlet)
+    # print(len(yy))
+    # print(np.mean(yy))
 
-for t in data['timestamp']:
-    # print(t)
-    xx.append(t)
-# print(len(xx))
+    for t in data['timestamp']:
+        # print(t)
+        xx.append(t)
+    # print(len(xx))
 
-new_time = []
-new_data = []
+    new_time = []
+    new_data = []
 
 
-def read_stream():
-    for change in mycol_sim.watch([{
-        '$match': {
-            'operationType': {'$in': ['replace', 'insert']},
-            'fullDocument.business': 'Digital Media Centre'
+    def read_stream():
+        for change in mycol_sim.watch([{
+            '$match': {
+                'operationType': {'$in': ['replace', 'insert']},
+                'fullDocument.business': 'Digital Media Centre'
+            }
         }
-    }
-    ]
-    ):
-        x = change["fullDocument"]
-        i = x['data']
-        add_sg = i["sf1_2"] + i["sf2_2"] + i["ahu_out"] + i['sg1_1'] + i['sg2_2'] + i['sg3_2'] + i['sg4_2'] + i['sg5_2'] + i['sg6_2']
-        mean_sg = add_sg / 9
-        yy.append(mean_sg)
+        ]
+        ):
+            x = change["fullDocument"]
+            i = x['data']
+            add_sg = i["sf1_2"] + i["sf2_2"] + i["ahu_out"] + i['sg1_1'] + i['sg2_2'] + i['sg3_2'] + i['sg4_2'] + i['sg5_2'] + i['sg6_2']
+            mean_sg = add_sg / 9
+            yy.append(mean_sg)
 
-        time = x['timestamp']
-        xx.append(time)
+            time = x['timestamp']
+            xx.append(time)
 
-st = Thread(target=read_stream, args=())
-st.start()
-st.is_alive()
+    st = Thread(target=read_stream, args=())
+    st.start()
+    st.is_alive()
 
-@app.callback(
-    Output('live-graph-2', 'figure'),
-    [Input('graph-update', 'n_intervals')]
-)
-def update_graph_scatter(n):
-    ahu=yy
-    # print(ahu)
-    temp= xx
-
-    data = plotly.graph_objs.Scatter(
-        x=list(temp),
-        y=list(ahu),
-        name='Scatter',
-        mode='lines+markers'
+    @app.callback(
+        Output('live-graph-2', 'figure'),
+        [Input('graph-update', 'n_intervals')]
     )
-    layout = go.Layout(
-        # paper_bgcolor='#27293d',
-        # plot_bgcolor='rgba(0,0,0,0)',
-        xaxis=dict(range=[min(temp), max(temp)]),
-        yaxis=dict(range=[min(ahu), max(ahu)]),
-        # font=dict(color='white'),
+    def update_graph_scatter(n):
+        ahu=yy
+        # print(ahu)
+        temp= xx
 
-    )
+        data = plotly.graph_objs.Scatter(
+            x=list(temp),
+            y=list(ahu),
+            name='Scatter',
+            mode='lines+markers'
+        )
+        layout = go.Layout(
+            # paper_bgcolor='#27293d',
+            # plot_bgcolor='rgba(0,0,0,0)',
+            xaxis=dict(range=[min(temp), max(temp)]),
+            yaxis=dict(range=[min(ahu), max(ahu)]),
+            # font=dict(color='white'),
 
-    return {'data': [data], 'layout':layout}
+        )
+
+        return {'data': [data], 'layout':layout}
+
+else:
+    print("No Result")
 
